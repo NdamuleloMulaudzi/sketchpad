ac4a3eb7f19337826561b7e5fd3e4fbb49d6061b
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-08-03 13:51:16 +0200

    added button: to get a new rid size

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const container =document.querySelector("#container")
-
 let containerSize = 500;
+let nSquare=16;
+
+let container =document.querySelector("#container")
+const sizeBtn= document.querySelector("button")
+const body = document.querySelector("body")
+
+
+
+sizeBtn.addEventListener("click",()=>{
+    nSquare= Number(prompt("Enter the Grid Size (MAX:100)"));
+    while(nSquare<=0 || nSquare>100){
+        nSquare = Number(prompt("Invalid Size\nEnter Grid Size "))
+    }
+    container.remove();
+    container = document.createElement("div")
+    container.setAttribute("id", "container");
+    createGrid(nSquare)
+    body.appendChild(container)
+ })
+
+
 
 function createGrid(nSquare){
     for(let i=0;i<nSquare;i++){
@@ -11,9 +30,17 @@ function createGrid(nSquare){
             square.style.height = containerSize/nSquare + "px"
 
             container.appendChild(square)
+            
+            square.addEventListener("mouseenter", ()=>{
+                square.style.background = "black"
+            })
+            
         }
+        
     }
-    
+
 }
 
-createGrid(3)
+ createGrid(nSquare)
+ 
+ 
