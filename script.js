bdcd209bdd2e77e709513a27835d6c0178f9680f
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-08-03 15:16:23 +0200

    added logic for the clear button

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 let containerSize = 500;
 let nSquare=16;
 
+//get the neccessary DOM elements
 let container =document.querySelector("#container")
 const sizeBtn= document.querySelector("button")
 const body = document.querySelector("body")
+const sizeText = document.querySelector("#sizeText")
+const clearBtn = document.querySelector("#clearGrid")
 
-
-
+//event listener that allows one to enter a new number of squares
 sizeBtn.addEventListener("click",()=>{
     nSquare= Number(prompt("Enter the Grid Size (MAX:100)"));
     while(nSquare<=0 || nSquare>100){
-        nSquare = Number(prompt("Invalid Size\nEnter Grid Size "))
+        nSquare = (prompt("Invalid Size\nEnter Grid Size "))
     }
     container.remove();
-    container = document.createElement("div")
-    container.setAttribute("id", "container");
+    container = document.createElement("div");
+    container.setAttribute("id","container");
     createGrid(nSquare)
     body.appendChild(container)
+    sizeText.textContent = "Current Grid Size : "+nSquare+ " x "+nSquare
+    body.appendChild(sizeText)
+    
  })
 
+ 
 
-
+//Function that creates a box with squares 
 function createGrid(nSquare){
     for(let i=0;i<nSquare;i++){
         for(let j=0;j<nSquare;j++){
@@ -34,7 +40,9 @@ function createGrid(nSquare){
             square.addEventListener("mouseenter", ()=>{
                 square.style.background = "black"
             })
-            
+            clearBtn.addEventListener("click", ()=>{
+                square.style.background = "white"
+            })
         }
         
     }
@@ -43,4 +51,9 @@ function createGrid(nSquare){
 
  createGrid(nSquare)
  
- 
+ //basic styles
+ sizeText.setAttribute("style",
+    "font-size:20px")
+clearBtn.setAttribute("style",
+    "font-size:1.5em"
+)
